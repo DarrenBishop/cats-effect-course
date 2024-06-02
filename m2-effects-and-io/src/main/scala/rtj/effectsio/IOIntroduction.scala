45122885140aca5314b4d18ff7252edc838ad8be
Darren Bishop <mail@darrenbishop.com>
2024-06-02 16:22:50 +0100

    Effects and IO - IO: Exercise Answers
    Signed-off-by: Darren Bishop <mail@darrenbishop.com>

--- m2-effects-and-io/src/main/scala/rtj/effectsio/IOIntroduction.scala
+++ m2-effects-and-io/src/main/scala/rtj/effectsio/IOIntroduction.scala
@@ -45,43 +45,100 @@ object IOIntroduction {
   }
 
   /**
-   * Exercises
+   * exercises
    */
 
   // 1 - sequence two IOs and take the result of the last one
   // hint: use flatMap
-  def sequenceTakeLast[A, B](ioa: IO[A], iob: IO[B]): IO[B] = ???
+  def sequenceTakeLast[A, B](ioa: IO[A], iob: IO[B]): IO[B] =
+    ioa.flatMap(_ => iob)
+
+  def sequenceTakeLast_v2[A, B](ioa: IO[A], iob: IO[B]): IO[B] = for
+    _ <- ioa
+    b <- iob
+  yield b
+
+  def sequenceTakeLast_v3[A, B](ioa: IO[A], iob: IO[B]): IO[B] =
+    ioa *> iob // "andThen"
+
+  def sequenceTakeLast_v4[A, B](ioa: IO[A], iob: IO[B]): IO[B] =
+    ioa >> iob // "andThen" with by-name iob i.e. lazy
 
   // 2 - sequence two IOs and take the result of the first one
   // hint: use flatMap
-  def sequenceTakeFirst[A, B](ioa: IO[A], iob: IO[B]): IO[A] = ???
+  def sequenceTakeFirst[A, B](ioa: IO[A], iob: IO[B]): IO[A] =
+    ioa.flatMap(a => iob.map(_ => a))
+
+  def sequenceTakeFirst_v2[A, B](ioa: IO[A], iob: IO[B]): IO[A] = for
+    a <- ioa
+    _ <- iob
+  yield a
+
+  def sequenceTakeFirst_v3[A, B](ioa: IO[A], iob: IO[B]): IO[A] =
+    ioa <* iob
 
   // 3 - repeat an IO effect forever
   // hint: use flatMap
-  def forever[A](io: IO[A]): IO[A] = ???
+  def forever[A](io: IO[A]): IO[A] = io.flatMap(_ => forever(io))
+
+  def forever_v2[A](io: IO[A]): IO[A] = io >> forever_v2(io) // same
+
+  def forever_v3[A](io: IO[A]): IO[A] = io *> forever_v3(io) // same, but not stack-safe
+
+  def forever_v4[A](io: IO[A]): IO[A] = io.foreverM
 
   // 4 - convert an IO to a different type
   // hint: use map
-  def convert[A, B](ioa: IO[A], value: B): IO[B] = ???
+  def convert[A, B](ioa: IO[A], value: B): IO[B] = ioa.map(_ => value)
+
+  def convert_v2[A, B](ioa: IO[A], value: B): IO[B] = ioa.as(value)
 
   // 5 - discard value inside an IO, just return unit
-  def discard[A](ioa: IO[A]): IO[Unit] = ???
+  def discard[A](ioa: IO[A]): IO[Unit] = convert(ioa, ())
+  def discard_v1[A](ioa: IO[A]): IO[Unit] = ioa.as(())
+  def discard_v2[A](ioa: IO[A]): IO[Unit] = ioa.void
 
   // 6 - fix stack recursion
   def sum(n: Int): Int =
     if n <= 0 then 0
     else n + sum(n - 1)
 
-  def sumIO(n: Int): IO[Int] = ???
+  def sumIO(n: Int): IO[Int] = IO(n).flatMap {
+    case n if n <= 0 => IO(n)
+    case n           => sumIO(n - 1).map(_ + n)
+  }
 
   // 7 (hard) - write fibinacci IO that does NOT crash on recursion
   // hints: use recursion, ignore exponential complexity, use flatMap heavily
-  def fibonacci(n: Int): IO[BigInt] = ???
+  def fibonacci(n: Int): IO[BigInt] = {
+    def aux(n: Int): IO[(BigInt, BigInt)] = IO(n).flatMap {
+      case 0 => IO((BigInt(0), BigInt(0)))
+      case 1 => IO((BigInt(1), BigInt(0)))
+      case _ => aux(n - 1).flatMap {
+        case (l1, l2) => IO((l1 + l2, l1))
+      }
+    }
+
+    aux(n).map(_._1)
+  }
 
   def main(args: Array[String]): Unit = {
     import cats.effect.unsafe.implicits.global // platform
     // The 'END of the World!"
 
-    runExamples
+    //runExamples
+
+    println(sequenceTakeLast(IO("first"), IO("last")).unsafeRunSync())
+    println(sequenceTakeFirst(IO("first"), IO("last")).unsafeRunSync())
+    //println(forever(IO(println("Hello!"))).unsafeRunSync())
+    println(convert(IO("Hello!"), "Goodbye!".toList).unsafeRunSync())
+    println(discard(IO("Hello!")).unsafeRunSync())
+
+    val summed = 100000
+    //println(s"sum($summed): ${sum(summed)}")
+    println(s"sumIO($summed): ${sumIO(summed).unsafeRunSync()}")
+    List(1, 10, 100, 1000, 10_000, 100_000).foreach { n =>
+      println(s"IO Fibonacci $n: ${fibonacci(n).unsafeRunSync()}")
+    }
   }
 }
