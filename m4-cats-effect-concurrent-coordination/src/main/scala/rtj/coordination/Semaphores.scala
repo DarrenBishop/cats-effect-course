726a3b6085f9ffb9675c9bdb7169939ac2346598
Darren Bishop <mail@darrenbishop.com>
2025-05-16 10:12:00 +0100

    Cats Effect: Concurrent Coordination - Semaphores: Exercise Answers
    Signed-off-by: Darren Bishop <mail@darrenbishop.com>

--- m4-cats-effect-concurrent-coordination/src/main/scala/rtj/coordination/Semaphores.scala
+++ m4-cats-effect-concurrent-coordination/src/main/scala/rtj/coordination/Semaphores.scala
@@ -60,31 +60,49 @@ object Semaphores extends IOApp.Simple {
 
   /**
    * Exercise
-   *   1. find out if there's something wrong with this code
-   *   2. why?
+   *   1. find out if there's something wrong with this code? yes, they all run and complete concurrently
+   *   2. why? the semaphore/mutex is not shared; it's an IO (effect), not a value
    *   3. fix it
    */
-  
+
   // Semaphore with 1 permit == mutex
   val mutex = Semaphore[IO](1)
-  
-  val users: IO[List[Int]] = random[IO].flatMapU {
+
+  val users: IO[List[Int]] = random[IO].flatMapU { rng ?=>
     (1 to 10).toList.parTraverse { id =>
       for {
         sem <- mutex
-        _ <- IO.dbg(s"[session $id] waiting to log-in...")
+        _ <- IO.dbg(s"[session $id; sem $sem] waiting to log-in...")
         _ <- sem.acquire
         // critical section start
-        _ <- IO.dbg(s"[session $id] logged in, working...")
+        _ <- IO.dbg(s"[session $id; sem $sem] logged in, working...")
         result <- doWorkWhileLoggedIn
-        _ <- IO.dbg(s"[session $id] done: $result, logging out...")
+        _ <- IO.dbg(s"[session $id; sem $sem] done: $result, logging out...")
         // critical section end
         _ <- sem.release
       } yield result
     }
   }
 
+  val usersFixed: IO[List[Int]] = random[IO].flatMapU {
+    mutex.flatMap { sem =>
+      (1 to 10).toList.parTraverse { id =>
+        for {
+          _ <- IO.dbg(s"[session $id; sem $sem] waiting to log-in...")
+          _ <- sem.acquire
+          // critical section start
+          _ <- IO.dbg(s"[session $id; sem $sem] logged in, working...")
+          result <- doWorkWhileLoggedIn
+          _ <- IO.dbg(s"[session $id; sem $sem] done: $result, logging out...")
+          // critical section end
+          _ <- sem.release
+        } yield result
+      }
+    }
+  }
+
   //def run: IO[Unit] = demoSemaphore()
   //def run: IO[Unit] = demoWeightedSemaphore()
-  def run: IO[Unit] = users.dvoid
+  //def run: IO[Unit] = users.dvoid
+  def run: IO[Unit] = usersFixed.dvoid
 }
