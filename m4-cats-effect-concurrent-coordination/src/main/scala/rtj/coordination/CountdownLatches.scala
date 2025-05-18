5721a23e5ef888bb4b08e6d0935b4c2ec2e7a1ba
Darren Bishop <mail@darrenbishop.com>
2025-05-18 09:18:28 +0100

    Cats Effect: Concurrent Coordination - Count Down Latches
    Signed-off-by: Darren Bishop <mail@darrenbishop.com>

--- m4-cats-effect-concurrent-coordination/src/main/scala/rtj/coordination/CountdownLatches.scala
+++ m4-cats-effect-concurrent-coordination/src/main/scala/rtj/coordination/CountdownLatches.scala
@@ -1,7 +1,7 @@
 package rtj.coordination
 
 import cats.effect.std.CountDownLatch
-import cats.effect.{Deferred, Fiber, IO, IOApp, Outcome, OutcomeIO, Ref}
+import cats.effect.{IO, IOApp}
 import cats.syntax.all.*
 import rtj.all.*
 
@@ -13,33 +13,33 @@ import rtj.all.*
  */
 object CountdownLatches extends IOApp.Simple {
 
-def announcer(latch: CountDownLatch[IO]): IO[Unit] = for {
-  _ <- IO.pause("Starting race shortly...", 2.seconds)
-  _ <- IO.pause("5...")
-  _ <- latch.release
-  _ <- IO.pause("4...")
-  _ <- latch.release
-  _ <- IO.pause("3...")
-  _ <- latch.release
-  _ <- IO.pause("2...")
-  _ <- latch.release
-  _ <- IO.pause("1...")
-  _ <- latch.release // gun firing
-  _ <- IO.dbg("GO GO GO!")
-} yield ()
+  def announcer(latch: CountDownLatch[IO]): IO[Unit] = for {
+    _ <- IO.pause("Starting race shortly...", 2.seconds)
+    _ <- IO.pause("5...")
+    _ <- latch.release
+    _ <- IO.pause("4...")
+    _ <- latch.release
+    _ <- IO.pause("3...")
+    _ <- latch.release
+    _ <- IO.pause("2...")
+    _ <- latch.release
+    _ <- IO.pause("1...")
+    _ <- latch.release // gun firing
+    _ <- IO.dbg("GO GO GO!")
+  } yield ()
 
-def craeteRunner(id: Int, latch: CountDownLatch[IO]): IO[Unit] = for {
-  _ <- IO.dbg(s"[runner $id] waiting for signal...")
-  _ <- latch.await // block the fiber until the count reaches 0
-  _ <- IO.dbg(s"[runner $id] RUNNING!")
-} yield ()
+  def craeteRunner(id: Int, latch: CountDownLatch[IO]): IO[Unit] = for {
+    _ <- IO.dbg(s"[runner $id] waiting for signal...")
+    _ <- latch.await // block the fiber until the count reaches 0
+    _ <- IO.dbg(s"[runner $id] RUNNING!")
+  } yield ()
 
-def sprint(): IO[Unit] = for {
-  latch <- CountDownLatch[IO](5)
-  announcerFib <- announcer(latch).start
-  _ <- (1 toL 10).parTraverse(craeteRunner(_, latch))
-  _ <- announcerFib.join
-} yield ()
+  def sprint(): IO[Unit] = for {
+    latch <- CountDownLatch[IO](5)
+    announcerFib <- announcer(latch).start
+    _ <- (1 toL 10).parTraverse(craeteRunner(_, latch))
+    _ <- announcerFib.join
+  } yield ()
 
   def run: IO[Unit] = sprint()
 }
