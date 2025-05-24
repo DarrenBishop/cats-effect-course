49589af673daf270e9b0ff15cb10f595633e4805
Darren Bishop <mail@darrenbishop.com>
2025-05-24 14:30:39 +0100

    Cats Effect: Polymorphic Effects - Polymorphic Coordination: Exercise Answer II
    Signed-off-by: Darren Bishop <mail@darrenbishop.com>

--- m5-polymorphic-effects/src/main/scala/rtj/polymorphic/PolymorphicCoordination.scala
+++ m5-polymorphic-effects/src/main/scala/rtj/polymorphic/PolymorphicCoordination.scala
@@ -1,19 +1,77 @@
 package rtj.polymorphic
 
-import cats.{Applicative, FlatMap, Functor, Monad}
+import cats.{Applicative, FlatMap, Functor, Monad, Parallel}
+import cats.effect.Outcome.{Canceled, Errored, Succeeded}
 import cats.effect.{Concurrent, Deferred, Fiber, IO, IOApp, MonadCancel, MonadCancelThrow, Outcome, Poll, Ref, Spawn}
 import cats.syntax.all.*
 import cats.effect.syntax.all.*
 import rtj.all.*
 
+import scala.collection.immutable.Queue
+import scala.util.Random
+
 object PolymorphicCoordination extends IOApp.Simple {
 
   // Concurrent - Ref + Deferred for ANY effect type
   object my {
-    trait Concurrent[F[_]] extends Spawn[F] {
-      def ref[A](a: A): F[Ref[F, A]]
-      def deferred[A]: F[Deferred[F, A]]
+    //trait Concurrent[F[_]] extends Spawn[F] {
+    //  def ref[A](a: A): F[Ref[F, A]]
+    //  def deferred[A]: F[Deferred[F, A]]
+    //}
+
+    trait Mutex[F[_]] {
+      def acquire: F[Unit]
+      def release: F[Unit]
     }
+
+    object Mutex {
+      type Signal[F[_]] = Deferred[F, Unit]
+
+      case class State[F[_]](locked: Boolean, signals: Queue[Signal[F]])
+
+      private def unlocked[F[_]]: State[F] = State(false, Queue())
+
+      def create[F[_]: Debug](using C: Concurrent[F]): F[Mutex[F]] = C.ref(unlocked[F]).map { state =>
+        new Mutex[F] {
+          /*
+              Change the state of the Ref:
+              - if the mutex is currently unlocked, state becomes (true, [])
+              - if the mutex is locked, state becomes (true, queue + new-signal) AND WAIT ON THAT SIGNAL
+           */
+          def acquire: F[Unit] = C.uncancelable { poll =>
+            for {
+              signal <- C.deferred[Unit]
+              cleanup = state.flatModify {
+                case State(locked, queue) => State(locked, queue.filterNot(_ eq signal)) -> ("cleanup".pure.dbg >> release)
+              }
+              pf: (State[F] ?> (State[F], F[Unit])) = ?> {
+                case State(false, _) => State(true, Queue()) -> C.unit
+                case State(true, queue) => State(true, queue.enqueue(signal)) -> poll(signal.get).onCancel(cleanup)
+              }
+              //_ <- state.flatModify(pf) // hangs
+              _ <- state.modify(pf).flatten
+            } yield ()
+          }
+
+          /*
+              Change the state of the Ref:
+              - if the mutex is unlocked, leave the state unchanged
+              - if the mutex is locked:
+                - if the queue is empty, unlock the mutex i.e. state becomes (false, [])
+                - if the queue is not empty, remove a signal from the queue and complete it (thereby unblocing a fiber waiting on it)
+           */
+          def release: F[Unit] = state.flatModify {
+              case state @ State(false, _) => (state, C.unit)
+              case State(true, Queue()) => (State(false, Queue()), C.unit)
+              case State(true, queue) =>
+                val (next, rest) = queue.dequeue
+                (State(true, rest), next.complete(()).void)
+            }
+        }
+      }
+    }
+
+    def mutex[F[_]: {Concurrent, Debug}, A](f: Mutex[F] => F[A]): F[A] = Mutex.create.flatMap(f)
   }
 
   val concurrentIO = Concurrent[IO] // given instance if Concurrent[IO]
@@ -65,8 +123,43 @@ object PolymorphicCoordination extends IOApp.Simple {
     case Left(outA) => Left(outA -> fibb)
     case Right(outB) => Right(fiba -> outB)
   }
-  
+
+  def criticalTask[F[_]: {Monad, Sleep}]: F[Int] = Random.nextInt(100).pure.sleep(1.second)
+
+  val ids: List[Int] = 1 toL 10
+
+  def createLockingTask[F[_]: {Monad, Debug, Sleep}](id: Int, mutex: my.Mutex[F]): F[Int] = for {
+    _ <- s"[task $id] waiting for permission...".pure.dbg
+    _ <- mutex.acquire // blocks if the mutex has been acquired by some other fibre
+    // critical section start
+    _ <- s"[task $id] working...".pure.dbg
+    res <- criticalTask
+    _ <- s"[task $id] got result: $res".pure.dbg
+    // critical section end
+    _ <- s"[task $id] permissions relinquished".pure.dbg
+    _ <- mutex.release
+  } yield res
+
+  def createCancellingTask[F[_]: {Debug, Sleep}](id: Int, mutex: my.Mutex[F])(
+    using
+    C: Concurrent[F]
+  ): F[Int] =
+    if (id % 2 == 0) createLockingTask(id, mutex)
+    else for {
+      fib <- createLockingTask(id, mutex).onCancel(s"[task $id] received cancellation".pure.dvoid).start
+      _ <- C.unit.sleep(2.seconds) >> fib.cancel
+      out <- fib.join
+      result <- out match {
+        case Succeeded(effect) => effect
+        case Errored(_) => s"[task $id] errored".pure.dbg.as(-1)
+        case Canceled() => s"[task $id] canceled".pure.dbg.as(-2)
+      }
+    } yield result
+
+  def demoCancellingTask[F[_]: {Concurrent, Parallel, Debug, Sleep}]: F[List[Int]] = my.mutex { mtx => ids.parTraverse(createCancellingTask(_, mtx)) }
+
   //def run: IO[Unit] = polymorphicAlarm[IO]
-  def run: IO[Unit] = generalRacePair(IO.sleep(750.millis) >> IO("IO-A succeeded"), IO.sleep(500.millis) >> IO("IO-B succeeded")).dbg.void
+  //def run: IO[Unit] = generalRacePair(IO.sleep(750.millis) >> IO("IO-A succeeded"), IO.sleep(500.millis) >> IO("IO-B succeeded")).dbg.void
   //def run: IO[Unit] = generalRacePair(IO.sleep(1000.millis) >> !?("IO-A errored"), IO.sleep(500.millis) >> IO("IO-B succeeded")).dbg.start >>= (fib => IO.sleep(250.millis) >> fib.cancel)
+  def run: IO[Unit] = demoCancellingTask[IO].dvoid
 }
