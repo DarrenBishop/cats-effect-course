38242282a4275bb3953d02da6554b9a6bcd94870
Darren Bishop <mail@darrenbishop.com>
2025-05-24 16:18:44 +0100

    Cats Effect: Polymorphic Effects - Polymorphic Sync: Exercise Answers
    Signed-off-by: Darren Bishop <mail@darrenbishop.com>

--- m5-polymorphic-effects/src/main/scala/rtj/polymorphic/PolymorphicSync.scala
+++ m5-polymorphic-effects/src/main/scala/rtj/polymorphic/PolymorphicSync.scala
@@ -1,7 +1,11 @@
 package rtj.polymorphic
 
 import cats.Defer
-import cats.effect.{IO, IOApp, MonadCancel, Sync}
+import cats.effect.{IO, IOApp, MonadCancel, Resource, Sync}
+import cats.syntax.all.*
+import rtj.all.*
+
+import java.io.{BufferedReader, InputStreamReader}
 
 object PolymorphicSync extends IOApp.Simple {
 
@@ -54,8 +58,29 @@ object PolymorphicSync extends IOApp.Simple {
   }
 
   object Console {
-    def apply[F[_]: Sync]: Console[F] = ???
+    def apply[F[_]](using S: Sync[F]): F[Console[F]] = S.pure(System.in -> System.out).map { (in, out) =>
+      new Console[F] {
+        def println[A](a: A): F[Unit] = S.blocking(out.println(a))
+
+        /**
+         * There is a potential of problem hanging one if the threads from the blocking thread pool (or one of the CE threads)
+         *
+         * There is also `Sync.interruptible(true/false)`, which attempts to block the thread via thread interrupts in case of cancellation.
+         * The flag indicates whether you want the threa interrupt signals to be sent repeatedly (true) or not (false).
+         */
+        def readLine(): F[String] = Resource
+          .make(S.blocking { new BufferedReader(new InputStreamReader(in)) })(br => S.blocking { br.close() })
+          .use { br => S.blocking { br.readLine() }  }
+      }
+    }
   }
 
-  def run: IO[Unit] = ???
+  def demoConsole(): IO[Unit] = for {
+    console <- Console[IO]
+    _ <- console.println("Hi, what's your name?")
+    name <- console.readLine()
+    _ <- console.println(s"Hello $name, nice to meet you!")
+  } yield ()
+
+  def run: IO[Unit] = demoConsole()
 }
