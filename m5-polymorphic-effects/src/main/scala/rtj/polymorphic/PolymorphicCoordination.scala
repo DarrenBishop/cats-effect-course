4af2ff2ecd7f123f90e7e12c97859125c1410d93
Darren Bishop <mail@darrenbishop.com>
2025-05-24 13:28:30 +0100

    Cats Effect: Polymorphic Effects - Polymorphic Coordination: Exercise Questions
    Signed-off-by: Darren Bishop <mail@darrenbishop.com>

--- m5-polymorphic-effects/src/main/scala/rtj/polymorphic/PolymorphicCoordination.scala
+++ m5-polymorphic-effects/src/main/scala/rtj/polymorphic/PolymorphicCoordination.scala
@@ -1,7 +1,7 @@
 package rtj.polymorphic
 
 import cats.{Applicative, FlatMap, Functor, Monad}
-import cats.effect.{Concurrent, Deferred, Fiber, IO, IOApp, MonadCancel, MonadCancelThrow, Outcome, Poll, Ref, Spawn}
+import cats.effect.{Concurrent, Deferred, Fiber, IO, IOApp, MonadCancel, MonadCancelThrow, Outcome, OutcomeIO, Poll, Ref, Spawn}
 import cats.syntax.all.*
 import cats.effect.syntax.all.*
 import rtj.all.*
@@ -44,6 +44,31 @@ object PolymorphicCoordination extends IOApp.Simple {
       _ <- tick(time, signal)
     } yield ()
   }
+
+  /**
+   *    Exercise:
+   *    1. Generalize racePair
+   *    2. Generalize the Mutex primitive for any F
+   */
+
+  type RaceResult[F[_], A, B] = Either[
+    (Outcome[F, Throwable, A], Fiber[F, Throwable, B]), // (winner outcome, loser fiber)
+    (Fiber[F, Throwable, A], Outcome[F, Throwable, B]) // (loser fiber, winner outcome)
+  ]
+
+  type RaceResultIO[A, B] = RaceResult[IO, A, B]
+
+  def ourRacePair[A, B](ioa: IO[A], iob: IO[B]): IO[RaceResultIO[A, B]] = for {
+    race <- IO.deferred[Either[OutcomeIO[A], OutcomeIO[B]]].onCancel(IO.void("Deferred canceled!"))
+    fiba <- ioa.guaranteeCase(out => race.complete(Left(out)).void).onCancel(IO.void("IO-A canceled!")).start.onCancel(IO.void("IO-A-fiber canceled!"))
+    fibb <- iob.guaranteeCase(out => race.complete(Right(out)).void).onCancel(IO.void("IO-B canceled!")).start.onCancel(IO.void("IO-B-fiber canceled!"))
+    result <- race.get.onCancel(IO.void("IO-A vs IO-B race canceled!") >> (fiba.cancel, fibb.cancel).parTupled.void)
+  } yield result match {
+    case Left(outA) => Left(outA -> fibb)
+    case Right(outB) => Right(fiba -> outB)
+  }
   
-  def run: IO[Unit] = polymorphicAlarm[IO]
+  //def run: IO[Unit] = polymorphicAlarm[IO]
+  def run: IO[Unit] = ourRacePair(IO.sleep(750.millis) >> IO("IO-A succeeded"), IO.sleep(500.millis) >> IO("IO-B succeeded")).dbg.void
+  //def run: IO[Unit] = ourRacePair(IO.sleep(1000.millis) >> !?("IO-A errored"), IO.sleep(500.millis) >> IO("IO-B succeeded")).dbg.start >>= (fib => IO.sleep(250.millis) >> fib.cancel)
 }
